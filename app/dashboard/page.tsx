81750e7457032c5372668a17024916da2a0b15d0
FabioPianzi <fabiopianzi03@gmail.com>
2025-04-29 14:49:53 +0200

    aggiunta

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -79,6 +79,55 @@ export default function Dashboard() {
       
 =======
       <div className={styles.homeContainer}>
+
+        <div className={styles.helloContainer}>
+            <div className={styles.hello}>Hello,</div>
+            <div className={styles.hello}>User</div>
+        </div>
+    
+        <div className={styles.homeContainer2}>
+        
+            <div className={styles.allbuttonContainer}> 
+
+                <div className={styles.buttonContainer}>  
+
+                    <Link href="/storenotes">
+                        <button className={styles.button}>              
+                            <img className={styles.image} src="/appunti.svg" alt="store_notes"/>
+                            <div>Store Notes</div>
+                        </button>
+                    </Link>
+                    
+                    <Link href= "/summarize">
+                        <button className={styles.button}>
+                        <img className={styles.image} src="/libro.svg" alt="summarize"/>
+                        <div>Summarize</div>
+                        </button>
+                    </Link>
+                
+                </div>
+
+                <div className={styles.buttonContainer}> 
+
+                    <Link href="/flashcards">
+                        <button className={styles.button}>
+                            <img className={styles.image} src="/flashcard.svg" alt="flashcard"/>
+                            <div>Flashcards</div>
+                        </button>
+                    </Link>
+
+                    <Link href="/practice">
+                        <button className={styles.button}>
+                        <img className={styles.image} src="/dialogo.svg" alt="practice"/>
+                        <div>Practice</div>
+                        </button>
+                    </Link>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+>>>>>>> Stashed changes
     </div>
   );
 }
